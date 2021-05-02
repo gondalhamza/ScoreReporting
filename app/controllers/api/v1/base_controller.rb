b15a6ae52b4607dea413ba49779d15c7173a594e
Hamza Zubair <gondal.humza@gmail.com>
2021-05-02 21:18:45 +0200

    Json response

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BaseController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
+  include Response
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
+
+  private
+  def record_not_found_error
+    render json: { errors: ['Object was not found'] }, status: :not_found
+  end
 end
